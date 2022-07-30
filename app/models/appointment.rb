a1d25d4150a811142b3f29c7a583c6e543892ff8
Jude Davis <judedavis92@gmail.com>
2022-07-30 20:30:11 +0100

    Update appointment.rb model such that created_at/updated_at attributes can be updated.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,12 +1,25 @@
 # frozen_string_literal: true
 
 class Appointment < ApplicationRecord
-  before_create :parse_appointment_date
-  before_update :parse_appointment_date
+  before_create :before_appointment_creation
+  before_update :before_appointment_update
 
   validates :location_base, :appointment_date, :time_slot, presence: true
 
-  def parse_appointment_date
+  # Executed before creation and update
+  def set_appointment_date
     self.appointment_date = Date.parse(appointment_date.to_s)
+  end
+
+  def before_appointment_creation
+    set_appointment_date
+
+    self.created_at = Time.now
+  end
+
+  def before_appointment_update
+    set_appointment_date
+
+    self.updated_at = Time.now
   end
 end
