3a883d77b1b3a334bd61c9cfe24709b994b32871
Jude Davis <judedavis92@gmail.com>
2022-08-01 12:51:51 +0100

    Reverted changes because just learnt that t.timestaps manages created_at/updated_at automatically

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,25 +1,13 @@
 # frozen_string_literal: true
 
 class Appointment < ApplicationRecord
-  before_create :before_appointment_creation
-  before_update :before_appointment_update
+  before_create :set_appointment_date
+  before_update :set_appointment_date
 
   validates :location_base, :appointment_date, :time_slot, presence: true
 
   # Executed before creation and update
   def set_appointment_date
     self.appointment_date = Date.parse(appointment_date.to_s)
-  end
-
-  def before_appointment_creation
-    set_appointment_date
-
-    self.created_at = Time.now
-  end
-
-  def before_appointment_update
-    set_appointment_date
-
-    self.updated_at = Time.now
   end
 end
