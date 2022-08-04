f4c9e830a996b76ed5f6a991ccf0886db88b6772
Jude Davis <judedavis92@gmail.com>
2022-08-04 20:22:39 +0100

    Validate  attributes

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,8 @@
 class Person < ApplicationRecord
   belongs_to :appointment
+
+  validates :name, :duration
+
+  # Appointment attribute is non-null, so requires a presence
+  validates :appointment, presence true
 end
